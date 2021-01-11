db943c21cf2b49885328cf7cc7b60d825d8d7a23
ConnorMayberry <33204367+ConnorMayberry@users.noreply.github.com>
2021-01-11 09:56:20 -0600

    Update App.java
    Cloned Repo

--- hw1/src/main/java/edu/northwestern/ssa/App.java
+++ hw1/src/main/java/edu/northwestern/ssa/App.java
@@ -3,5 +3,6 @@ package edu.northwestern.ssa;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("first push");
     }
 }
